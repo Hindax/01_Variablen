9b0a9ae6725753b36bd628774b8dd43dd6ba0b46
Hindax <haar162@more2learn.de>
2022-11-08 12:33:25 +0100

    pap demo 1.variate

--- script_01.js
+++ script_01.js
@@ -10,7 +10,16 @@
 
 // Konsolen-Ausgaben
 // console.log("Hallo");
-// console.log(firstName);
+
 
 /** 02 Deklaration + Wertzuweisung** */
+
+// let firstName; // Deklaration (Definition)
+// firstName = "Max"; // Wertzuweisung
+// console.log(firstName); // Ausgabe
+
+// let familyName = "Mütze" ; // Deklaration + Wertzuweisung
+// console.log(familyName); // Ausgabe
+// console.log(firstName + " " + familyName);
+
 
