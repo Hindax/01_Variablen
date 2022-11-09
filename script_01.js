417d5faacbf62588da0d388e66271ea1d12e39b0
Hindax <hind.aarif@gmail.com>
2022-11-09 09:23:06 +0100

    constants

--- script_01.js
+++ script_01.js
@@ -14,14 +14,36 @@
 
 /** 02 Deklaration + Wertzuweisung** */
 
-let firstName; // Deklaration (Definition)
-firstName = "Max"; // Wertzuweisung
-console.log(firstName); // Ausgabe
+//let firstName; // Deklaration (Definition)
+///firstName = "Max"; // Wertzuweisung
+//console.log(firstName); // Ausgabe
 
-let familyName = "Mütze" ; // Deklaration + Wertzuweisung
-console.log(familyName); // Ausgabe
-console.log(firstName + " " + familyName);
+//let familyName = "Mütze" ; // Deklaration + Wertzuweisung
+//console.log(familyName); // Ausgabe
+//console.log(firstName + " " + familyName);
+
+//03 Deklaration + Wertziweisung II**
+
+////let firstName, familyName;
+//firstName = prompt("Bitte Vornamen eingeben");
+//familyName = prompt("Bitte Namen eingeben");
+//console.log(firstName + " " + familyName);
+
+/* Js ist eine untypisierte Sprache | untyped */
+
+// let test;
+// test = "Hi";
+// test = 2;
+// test = true;
+
+// console.log("Datentyp: " + typeof test);
+// console.log("Inhalt: " + test); //Ausgabe
+
+/**** 03 Konstanten */
 
 
+const test = "Hi"; // Variable mit Konstatnten Inhalt
+test = "Hallo"; // Keine neue Zuweisung zur LZ
+console.log("Inhalt: " + test); //Ausgabe
 
 
